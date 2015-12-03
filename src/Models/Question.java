237b6d6e8413ce03312b256fe7db4b8efc977554
Marco Ketelaars <Marco Ketelaars>
2015-12-03 14:58:50 +0100

    Add answer to question

--- src/Models/Question.java
+++ src/Models/Question.java
@@ -5,6 +5,7 @@ public class Question {
 	private int id;
 	private Round round;
 	private String text;
+	private Answer answer;
 	
 	public Question(int id, Round round, String text) {
 		this.id = id;
