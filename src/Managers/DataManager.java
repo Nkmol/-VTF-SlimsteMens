56e627ed65339452bf8637e4201e43beb0ee9835
mohwaili <mohammed.alwaili@gmail.com>
2015-12-05 10:49:40 +0100

    Get all players from the database

--- src/Managers/DataManager.java
+++ src/Managers/DataManager.java
@@ -1,16 +1,18 @@
 package Managers;
 
+import java.awt.List;
 import java.sql.*;
+import java.util.ArrayList;
+
+import Models.*;
 
 public class DataManager {
 
 	private static final String dbUrl = "jdbc:mysql://localhost/slimsteMens";
-	
 	private static final String username = "root";
 	private static final String password = "root";
 	
 	private static DataManager instance = null;
-	
 	private Connection connection;
 	
 	private DataManager() { 
@@ -27,6 +29,23 @@ public class DataManager {
 	public void test() {
 		Connection c = DataManager.getInstance().getConnection();
 		System.err.println("test");
+	}
+	
+	public ArrayList<Player> getAllPlayers() {
+		Connection c = DataManager.getInstance().getConnection();
+		ArrayList<Player> players = null;
+		try {
+			Statement statement = c.createStatement();
+			ResultSet data = statement.executeQuery("SELECT * from account as a "
+					+ "INNER JOIN accountrol AS ar ON a.naam = ar.account_naam");
+			players = new ArrayList<>();
+			while(data.next()) 
+				players.add(new Player(data));
+			
+		} catch (SQLException e) {
+		}
+		
+		return players;
 	}
 	
 	public Connection getConnection() {
