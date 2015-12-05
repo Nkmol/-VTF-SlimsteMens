d8b9d78e27413779c6b88c9c9f24d20cbd231737
mohwaili <mohammed.alwaili@gmail.com>
2015-12-05 10:28:11 +0100

    Establish a connection with the database (local)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import Managers.DataManager;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
+
+		DataManager.getInstance().test();
 
 	}
 
--- src/Managers/DataManager.java
+++ src/Managers/DataManager.java
@@ -1,10 +1,19 @@
 package Managers;
 
+import java.sql.*;
+
 public class DataManager {
 
+	private static final String dbUrl = "jdbc:mysql://localhost/slimsteMens";
+	
+	private static final String username = "root";
+	private static final String password = "root";
+	
 	private static DataManager instance = null;
 	
-	protected DataManager() {
+	private Connection connection;
+	
+	private DataManager() { 
 		
 	}
 	
@@ -13,6 +22,23 @@ public class DataManager {
 			instance = new DataManager();
 		}
 		return instance;
+	}
+	
+	public void test() {
+		Connection c = DataManager.getInstance().getConnection();
+		System.err.println("test");
+	}
+	
+	public Connection getConnection() {
+		try {
+			System.out.println("Connecting.....");
+			connection = DriverManager.getConnection(dbUrl, username, password);
+			System.out.println("Connected");
+		} catch (SQLException e) {
+			System.err.println("Connection Error: " + e.getMessage());
+		}
+		
+		return connection;
 	}
 	
 }
