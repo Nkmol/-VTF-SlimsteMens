f17cd419cb43b7942779211e48163e1f9de869c4
Marco Ketelaars <Marco Ketelaars>
2015-12-07 19:04:06 +0100

    Add MainFrame class

---
+++ src/View/MainFrame.java
@@ -0,0 +1,13 @@
+package View;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+
+	public MainFrame()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
