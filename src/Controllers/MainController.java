44dd13f27e942562c2609b8bed7ccf1945b79b43
Marco Ketelaars <Marco Ketelaars>
2015-12-07 19:08:08 +0100

    add MainFrame to MainController

--- src/Controllers/MainController.java
+++ src/Controllers/MainController.java
@@ -1,9 +1,16 @@
 package Controllers;
 
+import View.MainFrame;
+
 public class MainController {
 
+	private MainFrame mainFrame;
+	MainController() {
+		mainFrame = new MainFrame();
+	}
+	
 	public static void main(String[] args) {
-		
+		new MainController();
 	}
 
 }
