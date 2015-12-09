23e9e34942e19f67c9fb6bd3291f4ef2cc358de5
Marco Ketelaars <Marco Ketelaars>
2015-12-09 13:20:00 +0100

    Add method for checking player answer to Game class

--- src/Models/Game.java
+++ src/Models/Game.java
@@ -2,6 +2,8 @@ package Models;
 
 import java.util.ArrayList;
 
+import Utilities.StringUtility;
+
 public class Game {
 	
 	private int id;
@@ -11,9 +13,23 @@ public class Game {
 	private Round currentRound;
 	private ArrayList<ChatMessage> chatMessages;
 	
+	private final int MinimumAnswerPercentage = 80;
+	
 	public Game(Player player1, Player player2) {
 		this.player1 = player1;
 		this.player2 = player2;
+	}
+	
+	public boolean IsPlayerAnswerCorrect(PlayerAnswer player, Answer answer){
+		
+		if (StringUtility.CalculateMatchPercentage(player.getAnswer(), answer.getAnswer()) >=  MinimumAnswerPercentage)
+			return true;
+		
+		for (String Alternative : answer.getAlternatives())
+			if (StringUtility.CalculateMatchPercentage(player.getAnswer(), Alternative) >=  MinimumAnswerPercentage)
+				return true;
+		
+		return false;
 	}
 	
 }
