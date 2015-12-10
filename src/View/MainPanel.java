293d0898e330c9ce6b710203e965c07c7614235a
Marco Ketelaars <Marco Ketelaars>
2015-12-10 18:50:08 +0100

    Add panels to MainPanel

--- src/View/MainPanel.java
+++ src/View/MainPanel.java
@@ -1,13 +1,22 @@
 package View;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class MainPanel extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
+	private JPanel CategoryPanel;
+	private JPanel ActiveGamesPanel;
+	
 	public MainPanel() {
-
+		setLayout(new BorderLayout());
+		CategoryPanel = new CategoryPanel();
+		// TODO: change JPanel constructor to actual View for Active games
+		ActiveGamesPanel = new JPanel();
+		ActiveGamesPanel.setPreferredSize(new Dimension(100, getHeight()));
+		add(CategoryPanel, BorderLayout.CENTER);
+		add(ActiveGamesPanel, BorderLayout.EAST);
 	}
 
 }
