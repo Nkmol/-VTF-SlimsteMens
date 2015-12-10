79c1b03d82bac4d9233f6180fa9bd7ef9c12895e
Marco Ketelaars <Marco Ketelaars>
2015-12-10 16:22:19 +0100

    Change ReplayListPanel layout + add more test data

--- src/View/ReplayListPanel.java
+++ src/View/ReplayListPanel.java
@@ -1,5 +1,6 @@
 package View;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -14,27 +15,31 @@ public class ReplayListPanel extends JPanel {
 
 	public ReplayListPanel()
 	{
-		// with a GridLayout of 1 by 1 the element will be filling the panel
-		setLayout(new GridLayout(1,1));
-		
-		JScrollPane scrollPane = new JScrollPane();;
-		scrollPane.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		// TODO: get played games via databasemanager
 		ArrayList<ReplayListItem> list = new ArrayList<ReplayListItem>();
 		
 		// add listitems with temporary testdata
+		list.add(new ReplayListItem(3, "Temmie", false, 5, 150));
+		list.add(new ReplayListItem(3, "Marco", true, 5, 150));
+		list.add(new ReplayListItem(3, "Potato", true, 5, 42));
+		list.add(new ReplayListItem(3, "Aaron", true, 5, 150));
+		list.add(new ReplayListItem(3, "Temmie", false, 5, 150));
+		list.add(new ReplayListItem(3, "Temmie", false, 5, 150));
 		list.add(new ReplayListItem(1, "Vincent", false, 4, 2));
 		list.add(new ReplayListItem(2, "Temmie", false, 5, 150));
+		list.add(new ReplayListItem(3, "Temmie", false, 99, 42));
+		list.add(new ReplayListItem(3, "Temmie", false, 7, 150));
 		list.add(new ReplayListItem(3, "Temmie", false, 5, 150));
 		
+		Dimension PrefferedItemDimension = new Dimension(getWidth(), 200);
 		for(ReplayListItem item : list)
 		{
 			item.addMouseListener(new ReplayMouseAdapter());
-			scrollPane.add(item);
+			item.setPreferredSize(PrefferedItemDimension);
+			add(item);
 		}
-		
-		add(scrollPane);
 	}
 	
 	private class ReplayMouseAdapter extends MouseAdapter {
