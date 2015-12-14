1e768cb92b09eced37963e331f543da68da6f8c3
Marco Ketelaars <Marco Ketelaars>
2015-12-14 19:22:34 +0100

    Add accessor methods to MainPanel

--- src/View/MainPanel.java
+++ src/View/MainPanel.java
@@ -6,20 +6,50 @@ import javax.swing.JPanel;
    
 public class MainPanel extends JPanel {
 	
-	private JPanel CategoryPanel;
-	private JPanel ActiveGamesPanel;
+	private CategoryPanel categoryPanel;
+	private JPanel sidebarPanel;
 	
-	/**
-	 * Create the panel. 
-	 */
 	public MainPanel() {
 		setLayout(new BorderLayout());
-		CategoryPanel = new CategoryPanel();
-		// TODO: change JPanel constructor to actual View for Active games
-		ActiveGamesPanel = new JPanel();
-		ActiveGamesPanel.setPreferredSize(new Dimension(100, getHeight()));
-		add(CategoryPanel, BorderLayout.CENTER);
-		add(ActiveGamesPanel, BorderLayout.EAST);
+		
+		categoryPanel = new CategoryPanel();
+		sidebarPanel = new JPanel();
+		sidebarPanel.setPreferredSize(new Dimension(100, getHeight()));
+		
+		add(categoryPanel, BorderLayout.CENTER);
+		add(sidebarPanel, BorderLayout.EAST);
 	}
-
+	
+	public MainPanel(JPanel sidebarPanel, JPanel categoryPanel) {
+		setLayout(new BorderLayout());
+		
+		this.categoryPanel = new CategoryPanel(categoryPanel); 
+		this.sidebarPanel = sidebarPanel;
+		this.sidebarPanel.setPreferredSize(new Dimension(100, getHeight()));
+		
+		add(this.categoryPanel, BorderLayout.CENTER);
+		add(this.sidebarPanel, BorderLayout.EAST);
+	}
+	
+	public void setCategoryPanel(JPanel newPanel){
+		categoryPanel.setPanel(newPanel);
+	}
+	
+	public JPanel getCategoryPanel() {
+		return categoryPanel.getPanel();
+	}
+	
+	public void setSidebar(JPanel newPanel) {
+		remove(sidebarPanel);
+		sidebarPanel = newPanel;
+		if (newPanel != null)
+		{
+			this.sidebarPanel.setPreferredSize(new Dimension(100, getHeight()));
+			add(sidebarPanel, BorderLayout.EAST);
+		}
+	}
+	
+	public JPanel getSidebar() {
+		return sidebarPanel;
+	}
 }
