b020b97b28193b5a3f6fff153f6a4f56ee11163a
Marco Ketelaars <Marco Ketelaars>
2015-12-15 19:03:48 +0100

    Add ReplayPanel class

---
+++ src/View/ReplayPanel.java
@@ -0,0 +1,13 @@
+package View;
+
+import javax.swing.JPanel;
+
+import Controllers.ReplayController;
+
+public class ReplayPanel extends JPanel {
+
+	ReplayController controller;
+	public ReplayPanel(ReplayController controller) {
+		this.controller = controller;
+	}
+}
