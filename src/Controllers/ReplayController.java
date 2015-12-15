3e60f43e779578268418bc63fb5c9737942096aa
Marco Ketelaars <Marco Ketelaars>
2015-12-15 19:05:10 +0100

    Connect replayController and View

--- src/Controllers/ReplayController.java
+++ src/Controllers/ReplayController.java
@@ -1,5 +1,14 @@
 package Controllers;
 
-public class ReplayController {
+import View.ReplayPanel;
 
+public class ReplayController {
+	MainController parent;
+	ReplayPanel View;
+	
+	public ReplayController(int id, MainController parent) {
+		this.parent = parent;
+		View = new ReplayPanel(this);
+		parent.SetViewCategoryPanel(View);
+	}
 }
