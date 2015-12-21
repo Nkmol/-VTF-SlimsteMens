9b4b5c8569374f49bb8d74b2846477bc34c04168
Marco Ketelaars <Marco Ketelaars>
2015-12-22 00:58:22 +0100

    Add components to PuzzlePanel

--- src/View/PuzzlePanel.java
+++ src/View/PuzzlePanel.java
@@ -1,10 +1,13 @@
 package View;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import Controllers.PuzzleController;
 import Models.Puzzle;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 @SuppressWarnings("serial")
 public class PuzzlePanel extends JPanel {
@@ -13,16 +16,31 @@ public class PuzzlePanel extends JPanel {
 	
 	private JTextField txtQuestion;
 	
+	private JLabel[] lblAnswerPositions;
+	private JLabel[] lblAnswers;
+	
 	public PuzzlePanel(PuzzleController controller) {
 		this.controller = controller;
+		setLayout(new GridLayout(5, 5));
+		txtQuestion = new JTextField();
+		int AnswerAmount = 3;
+		lblAnswerPositions = new JLabel[AnswerAmount];
+		lblAnswers = new JLabel[AnswerAmount];
+		for (int I = 0; I < AnswerAmount; I++) {
+			lblAnswerPositions[I] = new JLabel('A' + I + ":");
+			lblAnswers[I] = new JLabel();
+		}
+		
 	}
 	
 	// Observer method for type safety
 	public void Observe(Puzzle puzzle) {
-		puzzle.addObserver((obs, o) -> Update((Puzzle)obs, o));
+		puzzle.addObserver((obs, o) -> Update((Puzzle)obs));
+		Update(puzzle);
 	}
 	
-	private void Update(Puzzle puzzle, Object o) {
-		puzzle.getQuestions();
+	private void Update(Puzzle puzzle) {
+		txtQuestion.setText(puzzle.getTurn().getQuestion().getText());
+		
 	}
 }
