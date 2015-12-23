f94a923e37ac4d67af89ab1da8362c58b0d322bc
Nkmol <sandermol95@hotmail.com>
2015-12-23 20:37:16 +0100

    fix gamePanel exception

--- src/View/GamePanel.java
+++ src/View/GamePanel.java
@@ -73,9 +73,11 @@ public class GamePanel extends JPanel implements Observer{
 		playerGame2 = model.getPlayerGame2();
 		
 		//TODO: Not sure which round i should choose to determ the round
-		//lblRoundType.setText(model.getRounds().get(0).getRoundType().toString()); // TODO fix Exception in thread "AWT-EventQueue-0" java.lang.IndexOutOfBoundsException: Index: 0, Size: 0
-		//lblPlayer1.setText(playerGame1.getPlayer().getName() + " : " + playerGame1.getTime());
-		//lblPlayer2.setText(playerGame2.getPlayer().getName() + " : " + playerGame2.getTime());
+		
+		if(model.getRounds().size() > 0) 
+			lblRoundType.setText(model.getCurrentRound().getRoundType().toString()); // TODO fix Exception in thread "AWT-EventQueue-0" java.lang.IndexOutOfBoundsException: Index: 0, Size: 0
+		lblPlayer1.setText(playerGame1.getPlayer().getName() + " : " + playerGame1.getTime());
+		lblPlayer2.setText(playerGame2.getPlayer().getName() + " : " + playerGame2.getTime());
 	}
 	
 	public void setChatPanel(ChatMessageView chatPanel){
