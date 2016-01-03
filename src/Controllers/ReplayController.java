06d939c6d2d5cb7b532aa6bf670fe1c30d095856
Marco Ketelaars <Marco Ketelaars>
2016-01-03 03:09:03 +0100

    Move RoundController from Replay to ReplayController

--- src/Controllers/ReplayController.java
+++ src/Controllers/ReplayController.java
@@ -3,10 +3,13 @@ package Controllers;
 import Managers.DataManager;
 import Models.Game;
 import Models.Replay;
+import Models.ReplayThreeSixNine;
+import Models.Round;
 import View.ReplayPanel;
 
 public class ReplayController {
 	MainController parent;
+	RoundController currentRoundController;
 	ReplayPanel view;
 	Replay model; 
 	public ReplayController(int id, MainController parent) {
@@ -16,6 +19,9 @@ public class ReplayController {
 		model = new Replay(game.getId(), game.getPlayer1(), game.getPlayer2(), game.getGameState());
 		Utilities.ComponentUtility.addActionListener(view, "btnSubmit", (e) -> model.getCurrentRound().onSubmit(model.getCurrentAnswer()));
 		Utilities.ComponentUtility.addActionListener(view, "btnPass", (e) -> model.getCurrentRound().onPass());
+		Round currentRound = new ReplayThreeSixNine(model);
+		currentRoundController = new ThreeSixNineController(model, currentRound);
+		model.setCurrentRound(currentRound);
 	}
 	
 	public void ShowView() {
--- src/Models/Replay.java
+++ src/Models/Replay.java
@@ -1,41 +1,22 @@
 package Models;
 
-import java.sql.ResultSet;
-import java.util.Observer;
-
-import javax.swing.JPanel;
-
-import Controllers.ReplayListController;
-import Controllers.RoundController;
-import Controllers.ThreeSixNineController;
 import Managers.DataManager;
 
 public class Replay extends Game {
-	private RoundController currentRoundController;
 	private int AnswerNumber;
 	
 	public Replay(int gameId, Player player1, Player player2, GameState gameState) {
 		super(gameId, player1, player2, gameState);
 		rounds = DataManager.getInstance().getRounds(this);
 		AnswerNumber = 0;
-		Round currentRound = new ReplayThreeSixNine(this);
-		currentRoundController = new ThreeSixNineController(this, currentRound);
-	}
-	
-	public JPanel GetView() {
-		return currentRoundController.getView();
 	}
 	
 	public String getCurrentAnswer() {
-		return currentRoundController.getModel()
+		return getCurrentRound()
 				.currentTurn
 				.getCurrentQuestion()
 				.getAnswers()
 				.get(AnswerNumber)
 				.getAnswer();
-	}
-	
-	public void AddObserver(Observer o) {
-		currentRound.addObserver(o);
 	}
 }
